993d63ae98c5202cdaba54adeb461b0a2dd5d60a
Dadabrands <dadajoshua055@gmail.com>
2025-01-06 14:33:49 +0100

    refactor: complete validation folder and encapsulate options in a dedicated file
    - Finished implementing all required validation logic
    
    - Organized and structured the validation folder for better maintainability
    
    - Encapsulated reusable options into a separate dedicated file for modularity
    
    - Improved code readability and reusability across the application

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,15 @@
-import { IsString } from "class-validator";
+import { IsEmail, IsPhoneNumber, IsString, Length } from "class-validator";
 
 export class CreateUserDto {
-    @IsString()
+@Length(2,50)
 readonly name: string;
-@IsString()
+
+@IsEmail()
 readonly email: string;
-@IsString()
+
+@IsPhoneNumber("NG")
 readonly phone: string;
+
 @IsString()
 readonly password: string;
 }
